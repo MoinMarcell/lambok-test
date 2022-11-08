50b8d9016356af5b1b37b5a02eb720a8a1689e60
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-11-08 14:53:27 +0100

    Initial commit

---
+++ src/main/java/com/example/lamboktest/LambokTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.lamboktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambokTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambokTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lamboktest/LambokTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lamboktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambokTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
